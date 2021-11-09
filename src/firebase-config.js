ae444e62edfb4ec576fd688dafd871e0efd37f5a
lewlian <seanlewtengsiong@gmail.com>
2021-11-09 13:12:55 +0800

    adjusted config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
+import { config } from "dotenv";
+config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWw8MuetTG_CsvgQXzQsWMWTPgPy9FQ7E",
-  authDomain: "tzapac-faucet.firebaseapp.com",
-  projectId: "tzapac-faucet",
-  storageBucket: "tzapac-faucet.appspot.com",
-  messagingSenderId: "157987303570",
-  appId: "1:157987303570:web:430147c3cf34236f85f64b",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase
